3167130f3b37a6271345c407d059bec86d0118c2
Andrés Carrillo <iamandres17@gmail.com>
2025-08-09 12:28:53 -0500

    Initial commit with project default structure

---
+++ xpns/xpnsApp.swift
@@ -0,0 +1,17 @@
+//
+//  xpnsApp.swift
+//  xpns
+//
+//  Created by Andrés Carrillo on 9/08/25.
+//
+
+import SwiftUI
+
+@main
+struct xpnsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ xpnsTests/xpnsTests.swift
@@ -0,0 +1,17 @@
+//
+//  xpnsTests.swift
+//  xpnsTests
+//
+//  Created by Andrés Carrillo on 9/08/25.
+//
+
+import Testing
+@testable import xpns
+
+struct xpnsTests {
+  
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+  
+}
